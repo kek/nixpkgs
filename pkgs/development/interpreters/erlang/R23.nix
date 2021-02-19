870911635c59e95de875640aa5625125ead9c2da
Karl Eklund <localpart@gmail.com>
2021-02-19 01:52:38 +0100

    upgrade erlang

--- pkgs/development/interpreters/erlang/R23.nix
+++ pkgs/development/interpreters/erlang/R23.nix
@@ -3,8 +3,8 @@
 # How to obtain `sha256`:
 # nix-prefetch-url --unpack https://github.com/erlang/otp/archive/OTP-${version}.tar.gz
 mkDerivation {
-  version = "23.1.4";
-  sha256 = "16ssxmrgjgvzg06aa1l4wz9rrdjfy39k22amgabxwb5if1g8bg8z";
+  sha256 = "1vhxxsxzkqazi1dwpzbhkwy6adccf24yvzm50hs7p0rbv99mprjn";
+  version = "23.2.5";
 
   prePatch = ''
     substituteInPlace make/configure.in --replace '`sw_vers -productVersion`' "''${MACOSX_DEPLOYMENT_TARGET:-10.12}"
